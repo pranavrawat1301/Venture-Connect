a944762a1092fdb55c18b90e39494494b3f3ea1d
Pranav Rawat <144065572+pranavrawat1301@users.noreply.github.com>
2024-11-12 01:05:54 +0530

    Delete Venture Connect directory

--- Venture Connect/data.py
+++
@@ -1,9 +0,0 @@
-from data_synthesis import DataSynthesizer
-
-data = DataSynthesizer()
-
-startups_df = data.generate_startup_data(num_startups=200)
-investor_df = data.generate_investor_data(num_investors=50)
-
-data.save_data(startups_df , investor_df)
-
